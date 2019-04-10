8d7e28655989e95f705dae3f49e172943fad6aff
Karissa McKelvey <krmckelv@gmail.com>
2019-04-10 16:54:56 -0700

    get old and new data lengths

--- test.js
+++ test.js
@@ -1,37 +1,19 @@
 var OldOsmdb = require('osm-p2p')
 var path = require('path')
 var Mapeo = require('@mapeo/core')
-var pump = require('pump')
-var through = require('through2')
-var tape = require('tape')
-var equals = require('deep-equals')
 
-var schema = require('./schema')
 var OsmKappa = require('./kappa.js')
 
 module.exports = main
 
 function main (oldPath, kappaPath) {
   var osm = OldOsmdb(path.join(oldPath, 'data'))
-  var mapeo = new Mapeo(OsmKappa(path.join(kappaPath), 'data'))
-  var rs = osm.queryStream([[-Infinity, Infinity], [-Infinity, Infinity]])
-  tape('this is a test', function (t) {
-    var check = through.obj(function (raw, enc, next) {
-      var elm = schema.transformOldObservation(raw)
-      console.log(raw, elm)
-      mapeo.osm.get(elm.id, function (err, nodes) {
-        t.error(err)
-        console.log(nodes)
-        var node = nodes[0]
-        // t.same(elm.timestamp, node.timestamp)
-        // t.same(elm.refs, node.refs)
-        if (!equals(elm.tags, node.tags)) console.log(elm, node)
-        next()
-      })
-    })
-    pump(rs, check, function (err) {
-      t.error(err)
-      t.end()
+  var mapeo = new Mapeo(OsmKappa(path.join(kappaPath, 'data')))
+  osm.query([[-Infinity, Infinity], [-Infinity, Infinity]], function (err, oldData) {
+    if (err) throw err
+    mapeo.osm.query([-Infinity, -Infinity, Infinity, Infinity], function (err, newData) {
+      if (err) throw err
+      console.log(oldData.length, newData.length)
     })
   })
 }
